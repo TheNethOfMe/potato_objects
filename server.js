e97a7f58d033fe63a40e86440e90b53eaa475ebb
neth <nethwebdev@gmail.com>
2018-09-14 16:45:02 -0700

    ready for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://neth:123456@localhost:5432/kilovolt';
+const conString = process.env.DATABASE_URL || 'postgres://neth:123456@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
